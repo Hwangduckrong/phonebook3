b96f4022b9588a40915ca851cbf9df97c5113d83
황덕룡 <hwangsee12@naver.com>
2024-08-20 11:21:01 +0900

    2024-08-20

--- src/main/java/com/javaex/controller/phonebookController.java
+++ src/main/java/com/javaex/controller/phonebookController.java
@@ -1,8 +1,12 @@
 package com.javaex.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.javaex.vo.PersonVo;
 
 @Controller /* 1번 */
 public class phonebookController {
@@ -19,5 +23,27 @@ public class phonebookController {
 		System.out.println("PhonebookController.writeForm()");
 
 		return "/WEB-INF/views/writeForm.jsp";
+	}
+/*등록 */
+	@RequestMapping(value="/write",method={RequestMethod.GET,RequestMethod.POST})
+	public String write(@ModelAttribute PersonVo personVo){//자동으로 묶어줌
+		System.out.println(personVo);
+		return "";
+	}
+
+	@RequestMapping(value="/write2", method= {RequestMethod.GET,RequestMethod.POST})
+	public String write2(@RequestParam(value="name")String name, 
+						@RequestParam(value="hp") String hp ,
+						@RequestParam(value="company") String company) {
+		System.out.println("PhonebookController.write2()");
+		System.out.println(name);
+		System.out.println(hp);
+		System.out.println(company);
+		
+		PersonVo personVo = new PersonVo(name,hp,company);
+		System.out.println(personVo);
+		
+		return "";
+	//localhost:8888/phonebook/write?name=aaa&hp=123&company=02	
 	}
 }
